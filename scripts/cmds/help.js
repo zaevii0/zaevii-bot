cdf8d7734302f9a7528660b3e1b98968d9814700
zaevii0 <zaevii0@gmail.com>
2026-06-01 20:13:05 +0800

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -74,7 +74,7 @@ module.exports = {
 						const requiredRole = cmd.config.role !== undefined ? cmd.config.role : 0; 
 
 			return message.reply(
-				`☠️ 𝗖𝗢𝗠𝗠𝗔𝗡𝗗 𝗜𝗡𝗙𝗢 ☠️\n\n` +
+				`COMMAND INFO\n\n` +
 				`➥ Name: ${name}\n` +
 				`➥ Category: ${category || "Uncategorized"}\n` +
 				`➥ Description: ${desc}\n` +
@@ -89,7 +89,7 @@ module.exports = {
 		const formatCommands = (cmds) =>
 			cmds.sort().map((cmd) => `× ${cmd}`);
 
-		let msg = `━━━ zævii ━━━\n`;
+		let msg = `━━━ ZÆVII ━━━\n`;
 		const sortedCategories = Object.keys(categories).sort();
 		for (const cat of sortedCategories) {
 			const emoji = emojiMap[cat] || "➥";
