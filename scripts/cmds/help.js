3c52b17334083e2765eaf68318e29b6e57a64a7a
zaevii0 <zaevii0@gmail.com>
2026-06-01 20:14:06 +0800

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -89,7 +89,7 @@ module.exports = {
 		const formatCommands = (cmds) =>
 			cmds.sort().map((cmd) => `× ${cmd}`);
 
-		let msg = `━━━ ZÆVII ━━━\n`;
+		let msg = `[ ÆZ BOT ]\n`;
 		const sortedCategories = Object.keys(categories).sort();
 		for (const cat of sortedCategories) {
 			const emoji = emojiMap[cat] || "➥";
